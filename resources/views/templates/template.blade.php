ed964daf1bf32525c30f4ef167f27f73c9855a75
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 18:15:16 -0500

    Update template.blade.php

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -27,7 +27,7 @@
                 <main class="c-main">
                     @yield('content')
                 </main>
-                <div class="text-center">
+                <div class="text-center fixed-bottom">
                     <a onclick="setUtmSource(event, this)" href="https://www.pornhub.com/view_video.php?viewkey=ph5bad8d657eb59&utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860106.png"/></a>
                 </div>
             </div>

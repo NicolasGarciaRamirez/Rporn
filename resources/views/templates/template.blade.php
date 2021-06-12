a32463758beb5ef77e9e0dc9c80151de56aa2f31
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 17:50:12 -0500

    updeate

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -28,7 +28,7 @@
                     @yield('content')
                 </main>
                 <div>
-{{--                    @include('templates.footer')--}}
+                    <a onclick="setUtmSource(event, this)" href="https://www.pornhub.com/view_video.php?viewkey=ph5bad8d657eb59&utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860106.png"/></a>
                 </div>
             </div>
         </div>

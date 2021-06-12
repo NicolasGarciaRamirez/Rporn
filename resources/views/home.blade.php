df7190192e247fbcb272cb8a6dec0f84269f7f9b
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 18:03:10 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
             <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860092.png"/></a><br>
         </div>
         <div class=" d-flex flex-row flex-wrap mt-4">
-{{--            <iframe src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
-{{--            <iframe src="https://www.pornhub.com/embed/ph608c428e2e78f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+            <iframe src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
+            <iframe src="https://www.pornhub.com/embed/ph608c428e2e78f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
 {{--            <iframe src="https://www.pornhub.com/embed/ph60a7cb84bbf29" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
 {{--            <iframe src="https://www.pornhub.com/embed/ph60be073113ae5" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
 {{--            <iframe src="https://www.pornhub.com/embed/ph60b7bc1143070" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
@@ -32,15 +32,15 @@
 {{--            <div class="m-auto">--}}
 {{--                <iframe src="https://www.pornhub.com/embed/ph5dff80f820aef" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
 {{--            </div>--}}
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph60b7eec28ffe9" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph5ef5d85bd7c4c" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph5fe119907ae13" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph60b7eec28ffe9" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph5ef5d85bd7c4c" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph5fe119907ae13" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
         </div>
     </div>
 @endsection

1b6ed90dce04c612404c6e3bd5e9bc3a1faab5d2
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 17:04:35 -0500

    updarte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,5 @@
 
 @section('content')
     <div>
-        <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860092.png"/></a>
-    </div>
+        <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Square" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860030.png"/></a>    </div>
 @endsection

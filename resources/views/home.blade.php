561180e1522a75004a16562fc52205a396b54864
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 18:07:35 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,25 @@
             <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860092.png"/></a><br>
         </div>
         <div class=" d-flex flex-row flex-wrap mt-4">
+            <span class="text-white">Anal</span>
             <div>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph608c428e2e78f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60a7cb84bbf29" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
             </div>
+            <span class="text-white">Jovensitas</span>
             <div>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60be073113ae5" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60b7bc1143070" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60abea28a5de3" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
             </div>
+            <span class="text-white">Maduras</span>
             <div>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60b35e9a8f863" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60be045bb824b" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph6011ebff1954a" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
             </div>
+            <span class="text-white">Lesvianas</span>
             <div>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph606d83c6ec51f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph60014d37b7870" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>

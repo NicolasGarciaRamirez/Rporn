a07c8d165c07c08d6666c23389b80ff80e7e40d4
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 17:59:57 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,32 +6,32 @@
             <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860092.png"/></a><br>
         </div>
         <div class=" d-flex flex-row flex-wrap mt-4">
-            <iframe src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph608c428e2e78f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60a7cb84bbf29" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60be073113ae5" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60b7bc1143070" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60abea28a5de3" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60b35e9a8f863" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60be045bb824b" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph6011ebff1954a" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph606d83c6ec51f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <iframe src="https://www.pornhub.com/embed/ph60014d37b7870" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph5ea601e285b33" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph605d955abdc3d" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph606e652abedde" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph609a1497f2176" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
-            <div class="m-auto">
-                <iframe src="https://www.pornhub.com/embed/ph5dff80f820aef" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
-            </div>
+{{--            <iframe src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph608c428e2e78f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60a7cb84bbf29" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60be073113ae5" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60b7bc1143070" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60abea28a5de3" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60b35e9a8f863" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60be045bb824b" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph6011ebff1954a" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph606d83c6ec51f" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <iframe src="https://www.pornhub.com/embed/ph60014d37b7870" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph5ea601e285b33" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph605d955abdc3d" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph606e652abedde" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph609a1497f2176" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
+{{--            <div class="m-auto">--}}
+{{--                <iframe src="https://www.pornhub.com/embed/ph5dff80f820aef" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>--}}
+{{--            </div>--}}
             <div class="m-auto">
                 <iframe src="https://www.pornhub.com/embed/ph60b7eec28ffe9" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
             </div>

4cd4923f6761894f8521dd1d6c084c76a511820b
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-12 18:19:52 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="text-center">
             <a onclick="setUtmSource(event, this)" href="http://www.pornhub.com?utm_source=domain&utm_medium=banner-paid&utm_campaign=hubtraffic_danybigpen1" border="0" target="_blank"><img alt="Pornhub Header" border="0" src="https://www.hubtraffic.com/_affiliate/banners/1543860092.png"/></a><br>
         </div>
-        <div class=" d-flex flex-column flex-wrap m-auto p-5">
+        <div class=" d-flex flex-column flex-wrap m-auto">
             <span class="text-white font-weight-bold mb-2">Anal</span>
             <div>
                 <iframe class="m-auto" src="https://www.pornhub.com/embed/ph5f74e42d24f57" frameborder="0" width="300" height="200" scrolling="no" allowfullscreen></iframe>
